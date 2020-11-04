be2b571d3035271c9fc40cd2a1705a739e179ceb
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2020-11-04 19:40:22 +0100

    Add the elseifs

--- js/script.js
+++ js/script.js
@@ -69,3 +69,45 @@ if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'papier' && playerMove == 'kamień') {
+      printMessage('Przegrywasz!')
+  }
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'nożyce' && playerMove == 'papier') {
+      printMessage('Przegrywasz!')
+  }
+
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'kamień' && playerMove == 'nożyce') {
+      printMessage('Przegrywasz!')
+  }
+
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis! Spróbuj ponownie!');
+  }
+
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis! Spróbuj ponownie!');
+  }
+
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis! Spróbuj ponownie!');
+  }
+
+if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+    printMessage('Czyżby walkower? Wybierz którąś opcję!');
+  }
+
+if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+    printMessage('Czyżby walkower? Wybierz którąś opcję!');
+  }
+
+if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+    printMessage('Czyżby walkower? Wybierz którąś opcję!');
+  } 
